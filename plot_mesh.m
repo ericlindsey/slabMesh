fe372c555c0eb10c5e6a5793c8a2d5f45bbb2145
Eric Lindsey <ericlindsey@users.noreply.github.com>
2023-05-16 14:52:20 -0600

    update plot_mesh to allow passing a vector to use for the colors of each triangle

--- plot_mesh.m
+++ plot_mesh.m
@@ -1,9 +1,12 @@
-function plot_mesh(fname)
+function plot_mesh(fname,varargin)
     % plot blocks mesh file: assumed .mat file format, contains variables:
     % nc: int, number of vertices
     % nEl: int, number of triangles
     % c: (nc x 3) double, contains lon,lat,depth(negative) for each vertex
     % v: (nEl x 3) int, contains triples of vertex IDs for each triangle
+    %
+    % optional: vector containing values to use for triangle colors
+    % (default: fault depth)
     %
     % Eric Lindsey, Aug 2020
     
@@ -13,13 +16,18 @@ function plot_mesh(fname)
     y=c(:,2);
     z=c(:,3);
     
-    % get centroid depths for each triangle
-    depcol=zeros(nEl,1);
-    for i=1:nEl
-        depths=z(v(i,:));
-        depcol(i)=sum(depths)/3;
+    if nargin == 1
+        % use centroid depths for triangle colors
+        colorvec=zeros(nEl,1);
+        for i=1:nEl
+            depths=z(v(i,:));
+            colorvec(i)=sum(depths)/3;
+        end
+    else
+        colorvec = varargin{1};
     end
+        
     % plot triangles
-    trisurf(v,x,y,z,depcol)
+    trisurf(v,x,y,z,colorvec)
 
 end
